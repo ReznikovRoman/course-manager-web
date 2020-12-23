c6a3ce85c8e9a4035a3017d662e4640eff312119
ReznikovRoman <romanreznikov2002@yandex.ru>
2020-12-23 13:59:33 +0300

    Add tests to the Profile models from the accounts app

--- course_manager/accounts/tests/test_models.py
+++ course_manager/accounts/tests/test_models.py
@@ -106,6 +106,57 @@ class CustomUserModelTest(TestCase):
         self.assertIsNotNone(user.profile.address)
 
 
+class ProfileModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create new User
+        user = CustomUser.objects.create_user(
+            email='profile1@gmail.com',
+            username='profile1',
+            password='romanroman1',
+        )
+
+    def test_first_name_max_length(self):
+        profile = CustomUser.objects.get(pk=1).profile
+        max_length = profile._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 40)
+
+    def test_last_name_max_length(self):
+        profile = CustomUser.objects.get(pk=1).profile
+        max_length = profile._meta.get_field('last_name').max_length
+        self.assertEquals(max_length, 40)
+
+    def test_phone_max_length(self):
+        profile = CustomUser.objects.get(pk=1).profile
+        max_length = profile._meta.get_field('phone').max_length
+        self.assertEquals(max_length, 20)
+
+    def test_profile_pic_upload_folder(self):
+        profile = CustomUser.objects.get(pk=1).profile
+        profile_pic_upload_folder = profile._meta.get_field('profile_pic').upload_to
+        self.assertEquals(
+            profile_pic_upload_folder,
+            'images/profile_pics'
+        )
+
+    def test_profile_pic_url_default(self):
+        profile = CustomUser.objects.get(pk=1).profile
+        profile_pic_url = profile.profile_pic
+        self.assertEquals(
+            profile_pic_url,
+            'images/profile_pics/default_profile_pics/user_1.png'
+        )
+
+    def test_object_name_is_first_name_space_last_name(self):
+        profile = CustomUser.objects.get(pk=1).profile
+        setattr(profile, 'first_name', 'Roman')
+        setattr(profile, 'last_name', 'Reznikov')
+        profile.save()
+
+        profile_object_name = str(profile)
+        self.assertEquals(profile_object_name, 'Roman Reznikov')
+
 
 
 
