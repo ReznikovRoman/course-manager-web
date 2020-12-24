a8f1c49bf8dbf2d65d8ef73a9f91838f8627d96c
ReznikovRoman <romanreznikov2002@yandex.ru>
2020-12-24 17:01:59 +0300

    Add models to the 'courses' app

--- course_manager/courses/admin.py
+++ course_manager/courses/admin.py
@@ -1,3 +1,100 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+
+from . import models
+
+#####################################################################################################################
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('base_title', 'short_description', 'course_instances_count')
+    search_fields = ('base_title', )
+
+    def short_description(self, obj: models.Course):
+        return obj.description[:60]
+
+    def course_instances_count(self, obj: models.Course):
+        return obj.instances.count()
+
+
+class CourseInstanceAdmin(admin.ModelAdmin):
+    list_display = ('sub_title', 'base_course_link', 'min_mark', 'start_date', 'end_date', 'assignments_count')
+    search_fields = ('sub_title', 'course__base_title')
+
+    def base_course_link(self, obj: models.CourseInstance):
+        return mark_safe(
+            f"""<a href="{reverse('admin:courses_course_change', args=(obj.course.pk,))}">{obj.course}</a>"""
+        )
+
+    def assignments_count(self, obj: models.CourseInstance):
+        return obj.course_assignments.count()
+
+    base_course_link.short_description = 'base course'
+
+
+class EnrollAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course_instance', 'average_mark', )
+    exclude = ('USERNAME_FIELD', )
+    search_fields = ('student__email', )
+    readonly_fields = ('average_mark', )
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    ordering = ()
+
+
+class MarkAdmin(admin.ModelAdmin):
+    list_display = ('value', 'enroll_link',)
+    search_fields = ('enroll__course_instance__sub_title', 'enroll__student__email')
+
+    def enroll_link(self, obj: models.Mark):
+        return mark_safe(
+            f"""<a href="{reverse('admin:courses_enroll_change', args=(obj.enroll.pk,))}">{obj.enroll}</a>"""
+        )
+
+
+class CourseInstanceAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course_instance_link', 'start_date', 'end_date', 'is_completed')
+    ordering = ('is_completed', )
+    list_filter = ('course_instance', )
+    search_fields = ('course_instance__sub_title', 'course_instance__course__base_title')
+
+    def course_instance_link(self, obj: models.CourseInstanceAssignment):
+        return mark_safe(
+            f"""<a href="{reverse('admin:courses_courseinstance_change', args=(obj.course_instance.pk,))}">{obj.course_instance}</a>"""
+        )
+
+    course_instance_link.short_description = 'course instance'
+
+
+class PersonalAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'enroll_link', 'start_date', 'end_date', 'is_completed')
+    ordering = ('is_completed', )
+    search_fields = ('enroll__student__email', )
+    readonly_fields = ('enroll_link', )
+
+    def enroll_link(self, obj: models.PersonalAssignment):
+        return mark_safe(
+            f"""{obj.enroll.course_instance} | <a href="{reverse('admin:courses_enroll_change', args=(obj.enroll.pk, ))}">{obj.enroll.student.email}</a>"""
+        )
+
+    enroll_link.short_description = 'enroll_link'
+
+######################################################################################################################
+
+
+admin.site.register(models.Course, CourseAdmin)
+admin.site.register(models.CourseInstance, CourseInstanceAdmin)
+admin.site.register(models.Enroll, EnrollAdmin)
+admin.site.register(models.Mark, MarkAdmin)
+
+admin.site.register(models.CourseInstanceAssignment, CourseInstanceAssignmentAdmin)
+admin.site.register(models.PersonalAssignment, PersonalAssignmentAdmin)
+
+
+
+
+
--- course_manager/courses/models.py
+++ course_manager/courses/models.py
@@ -1,47 +1,108 @@
 from django.db import models
 
-from accounts.models import (CustomUser, Profile, Address)
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import Group
+
+from accounts import models as account_models
 
 
 #####################################################################################################################
-'''
+
 
 class Course(models.Model):
-    # TODO: Course Model
-    pass
+    base_title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.base_title} Course"
 
 
 class CourseInstance(models.Model):
-    # TODO: Course Instance Model
     course = models.ForeignKey(Course, related_name='instances', on_delete=models.CASCADE)
-    # students
-    # teachers
+
+    sub_title = models.CharField(max_length=100, null=True, blank=True)
+    min_mark = models.SmallIntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100),
+        ]
+    )
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.sub_title:
+            self.sub_title = self.course.base_title
+        super(CourseInstance, self).save(*args, **kwargs)
+
+    def __str__(self):
+        if self.sub_title == self.course.base_title:
+            return f"{self.course} - {self.pk}"
+        return f"{self.sub_title}"
 
 
 class Enroll(models.Model):
-    # TODO: Enroll Model
-    # TODO(*) : Mark Model
-    pass
+    course_instance = models.ForeignKey(CourseInstance, related_name='enrolls', on_delete=models.CASCADE)
+    student = models.ForeignKey('accounts.CustomUser', related_name='enrolls', on_delete=models.CASCADE)
+
+    @property
+    def average_mark(self):
+        marks = list(self.marks.filter(enroll=self).values_list('value', flat=True))
+        try:
+            return round(float(sum(marks) / len(marks)), 2)
+        except ZeroDivisionError:
+            return 0
+
+    def save(self, *args, **kwargs):
+        student_group = Group.objects.get(name='students')
+        student_group.user_set.add(self.student)
+        super(Enroll, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.course_instance} - {self.student}"
+
+
+class Mark(models.Model):
+    enroll = models.ForeignKey(Enroll, related_name='marks', on_delete=models.CASCADE)
+    value = models.SmallIntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100),
+        ]
+    )
+
+    def __str__(self):
+        return f"Mark: {self.value}"
+
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Task: {self.title}"
+
+
+class CourseInstanceAssignment(Assignment):
+    course_instance = models.ForeignKey(CourseInstance, related_name='course_assignments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Course Task: {self.title}"
+
+
+class PersonalAssignment(Assignment):
+    enroll = models.ForeignKey(Enroll, related_name='personal_assignments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Personal Task: {self.title}"
 
 
 # ==========================  ==========================  ==========================  ==========================  === #
 
 
-class Student(models.Model):
-    # TODO: Student Model
-    pass
-
-
-class Teacher(models.Model):
-    # TODO: Teacher Model
-    pass
-
-
-class Manager(models.Model):
-    # TODO: Manager Model
-    pass
-
-'''
 
 
 
